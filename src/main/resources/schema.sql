d64afec11e97164da2ec74ecf6ee043abf1cf0f0
Umbasa <butuzov_tema@yahoo.com>
2025-06-26 20:10:00 +0300

    12 финальный спринт

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -53,3 +53,18 @@ CREATE TABLE IF NOT EXISTS friendships (
     CHECK (requester_id <> addressee_id),
     CHECK (status IN ('pending', 'confirmed'))
 );
+
+INSERT INTO age_ratings (rating_id, rating_name)
+VALUES  (1,'G'),
+        (2,'PG'),
+        (3,'PG-13'),
+        (4,'R'),
+        (5,'NC-17');
+
+INSERT INTO genres (genre_id, name)
+VALUES  (1,'Комедия'),
+        (2,'Драма'),
+        (3,'Мультфильм'),
+        (4,'Триллер'),
+        (5,'Документальный'),
+        (6, 'Боевик' );
